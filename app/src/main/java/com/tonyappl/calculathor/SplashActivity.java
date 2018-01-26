cfda692f92b3c6689effef3456c03d7e2bcfc90f
TonyPepperony89 <rus.official@mail.ru>
2018-01-26 16:44:12 +0300

    add splashscreen

---
+++ app/src/main/java/com/tonyappl/calculathor/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.tonyappl.calculathor;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 4 * 1000);
+    }
+}
